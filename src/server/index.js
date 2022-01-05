364c8f6fa2da90a490f6e748ad8c01d606a83c49
Charles Vallance <charles@sharesies.com>
2022-01-06 11:33:14 +1300

    Adding cors

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,19 @@
-import express from 'express'
-import glob from 'glob'
-import fs from 'fs'
-import path from 'path'
+import express from "express"
+import glob from "glob"
+import fs from "fs"
+import path from "path"
+import cors from "cors"
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.redirect('/manifest.txt')
+app.use(cors())
+
+app.get("/", (req, res) => {
+    res.redirect("/manifest.txt")
 })
 
-app.get('/manifest.txt', (req, res) => {
-    glob("**/*.js", {cwd: 'out'}, (_, matches) => {
+app.get("/manifest.txt", (req, res) => {
+    glob("**/*.js", { cwd: "out" }, (_, matches) => {
         let fileRes = ""
         for (var file of matches) {
             fileRes += `/${file}\n`
@@ -19,22 +22,22 @@ app.get('/manifest.txt', (req, res) => {
     })
 })
 
-app.get('**/*.js', (req, res) => {
-    fs.readFile(path.join('out', req.path), function(err, data) {
+app.get("**/*.js", (req, res) => {
+    fs.readFile(path.join("out", req.path), function (err, data) {
         if (err) {
             res.sendStatus(404)
             return
         }
 
         // We want to do a replacement so bitburner can import
-        const regex = /(import.*from\s{1})[\'\"]\.\/(.*)[\'\"]\;/g;
+        const regex = /(import.*from\s{1})[\'\"]\.\/(.*)[\'\"]\;/g
         let resStr = data.toString()
-        var newRes = resStr.replaceAll(regex, "$1'\/$2.js';")
+        var newRes = resStr.replaceAll(regex, "$1'/$2.js';")
 
         res.send(newRes)
     })
 })
 
 app.listen(3000, () => {
-    console.log('The application is listening on port 3000!')
+    console.log("The application is listening on port 3000!")
 })
