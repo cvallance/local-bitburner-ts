27337a8eb0c088e24cbe8229e608c4f206ad8712
Charles Vallance <vallance.charles@gmail.com>
2022-01-06 10:07:11 +1300

    Filter for factions

--- src/scripts/server-paths.ts
+++ src/scripts/server-paths.ts
@@ -7,17 +7,24 @@ export async function main(ns: NS) {
 	let servers = getServersWithPath(ns)
 	if (ns.args.length){
 		const filter = ns.args[0] as string
-		servers = servers.filter(x => x.Server.hostname.includes(filter))
+		if (filter == 'factions') {
+			servers = servers.filter(x => x.Server.moneyMax == 0 && !x.Server.purchasedByPlayer && x.Server.hostname != 'darkweb')
+		}
+		else {
+			// Simple text match
+			servers = servers.filter(x => x.Server.hostname.includes(filter))
+		}
 	}
 
     const table = new Table(ns)
     table.addColumn('Hostname')
+    table.addColumn('HackLvl')
     table.addColumn('Path')
 
 	for (const serverDetails of servers) {
 		const hostname = serverDetails.Server.hostname
 		const path = serverDetails.Path.join(' -> ')
-		table.addRow(hostname, path)
+		table.addRow(hostname, serverDetails.Server.requiredHackingSkill, path)
 	}
 
 	table.sortBy('Hostname')
